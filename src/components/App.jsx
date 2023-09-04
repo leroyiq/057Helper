85ec0b55c81efd9858d85239b2b7802621dd981b
leroyiq <leroyiq6@gmail.com>
2023-09-04 20:00:01 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,28 @@
-import { DIV, H1 } from './App.styled';
+import { BUTTON, DIV, H1 } from './App.styled';
+import estate from '../pictures/LogoV2_rounded.png';
+import { FaGooglePlay, FaInternetExplorer, FaTelegram } from 'react-icons/fa';
 
 export const App = () => {
   return (
     <>
-      <DIV></DIV>
+      <DIV>
+        <img src={estate} alt="1" width="100px" />
+      </DIV>
+
       <H1>Твiй помiчник у пошуку нерухомостi.</H1>
-      <ul>
-        <li>
+      <ul style={{ margin: 40, padding: 0 }}>
+        <BUTTON>
+          <FaInternetExplorer />
           <a href="https://bit.ly/41y53Lj">Наш Сайт</a>
-        </li>
-        <li>
+        </BUTTON>
+        <BUTTON>
+          <FaTelegram />
           <a href="https://bit.ly/43B7DC7">Наш Telegram бот</a>
-        </li>
-        <li>
+        </BUTTON>
+        <BUTTON>
+          <FaGooglePlay />
           <a href="https://bit.ly/43xyj6N">Наш додаток Android</a>
-        </li>
+        </BUTTON>
       </ul>
     </>
   );
--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,25 +1,69 @@
 import { styled } from 'styled-components';
-import background from './LogoV2_rounded.png';
+import background from '../pictures/photo_2023-04-07_14-08-17.jpg';
 
 export const DIV = styled.div`
   background-image: url(${background});
-  /* border: 1px solid #3d1cf8; */
-  border-radius: 10px;
+
   height: 200px;
-  margin: 10px 10px;
-  background-position: 50% 10px;
+  /* margin: 10px 10px; */
+  /* background-position: 50% 10px; */
   background-size: 40%;
   background-repeat: no-repeat;
+  background-size: cover;
+  img {
+    position: absolute;
+    top: 12%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 `;
 
 export const H1 = styled.h1`
   display: block;
   text-align: center;
-  border: 1px solid black;
-  background-color: #3b7a57;
+
+  background-color: #7bac91;
   padding: 10px;
   -webkit-box-shadow: -10px 0px 13px -7px #000000, 10px 0px 13px -7px #000000,
     5px 5px 15px 5px rgba(0, 0, 0, 0);
   box-shadow: -10px 0px 13px -7px #3b7a57, 10px 0px 13px -7px #000000,
     5px 5px 15px 5px rgba(0, 0, 0, 0);
+  font-family: Verdana, Geneva, sans-serif;
+  font-size: 20px;
+  letter-spacing: 2px;
+  word-spacing: 2px;
+  color: #101a9e;
+  font-weight: normal;
+  text-decoration: none;
+  font-style: normal;
+  font-variant: small-caps;
+  text-transform: uppercase;
+  color: #616161;
+  background: #bababa;
+  text-shadow: #e0e0e0 1px 1px 0;
+  color: #616161;
+  background: #bababa;
+`;
+
+export const BUTTON = styled.li`
+  display: block;
+  box-shadow: 0px 10px 14px -7px #276873;
+  background: linear-gradient(to bottom, #599bb3 5%, #408c99 100%);
+  background-color: #599bb3;
+  border-radius: 5px;
+
+  cursor: pointer;
+  color: #ffffff;
+  font-family: Arial;
+  font-size: 18px;
+  padding: 5px 10px;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #3d768a;
+  margin-bottom: 15px;
+
+  a {
+    padding-left: 10px;
+    text-decoration: none;
+    color: white;
+  }
 `;
